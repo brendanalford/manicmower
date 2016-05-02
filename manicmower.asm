1470b6bfbf4c268cc1dd9e2fca8c53bd08e69b32
Brendan Alford <brendan.alford@gmail.com>
2016-05-03 00:04:26 +0100

    Working level construction and drawing

--- manicmower.asm
+++ manicmower.asm
@@ -28,6 +28,8 @@ init
 
   call get_key
 
+  xor a
+  ld (v_level), a
   call prepare_level
   call display_level
   ret
@@ -58,6 +60,10 @@ display_level_loop
   call z, display_wall
   cp FLOWERS
   call z, display_flowers
+  cp GNOME
+  call z, display_gnome
+  cp FUEL
+  call z, display_fuel
 
   ld a, (v_column)
   inc a
@@ -117,8 +123,49 @@ display_flowers_1
   call putchar_8
   ret
 
+display_gnome
 
+  ld a, ixl
+  bit 5, a
+  ld a, %00100001 ; green
+  jr z, display_gnome_2
+  ld a, %01100001 ; bright green
+
+display_gnome_2
+
+  ld (v_attr), a
+  ld a, 'j'
+  call putchar_8
+  ret
+
+display_fuel
+
+  ld a, ixl
+  bit 5, a
+  ld a, %00100001 ; green
+  jr z, display_fuel_2
+  ld a, %01100001 ; bright green
+
+display_fuel_2
+
+  ld (v_attr), a
+  ld a, 'l'
+  call putchar_8
+  ret
+
+; Prepares the level buffer with current level data.
+; Level number is taken from sysvar v_level.
 prepare_level
+
+  ld bc, 0
+  ld a, (v_level)
+  sla a
+  ld c, a
+
+  ld ix, level_1_data
+;  add ix, bc
+;  ld de, (ix)
+;  ld ix, de
 
 ; Cover entire playfield in grass at first
 
@@ -173,6 +220,135 @@ prepare_level_side_wall
   ld (hl), a
   ldir
 
+; Start reading level data (pointed to by IX)
+
+  ld b, 14
+  ld hl, level_buffer + (32 * 2) + 1
+
+prepare_flower_loop
+
+  push hl
+  ld de, 29
+  add hl, de
+  ld de, hl
+  pop hl
+  push hl
+
+  push bc
+  ld a, (ix)
+  cp 0
+  jr z, prepare_flower_next
+  ld b, a
+  ld a, FLOWERS
+
+prepare_flower_inner_loop
+
+  ld (hl), a
+  ex de, hl
+  ld (hl), a
+  ex de, hl
+  dec de
+  inc hl
+  djnz prepare_flower_inner_loop
+
+prepare_flower_next
+
+  pop bc
+  pop hl
+
+  ld de, 32
+  add hl, de
+  inc ix
+  djnz prepare_flower_loop
+
+; IX now points at wall X,Y positions
+
+prepare_interior_walls
+
+  ld hl, (ix)
+  inc ix
+  inc ix
+  ld a, l
+  or h
+  jr z, prepare_gnomes
+
+  call calc_xy_to_hl
+  ex de, hl
+  ld hl, level_buffer
+  add hl, de
+
+  ld b, 5
+  ld a, WALL
+
+prepare_interior_walls_loop
+
+  ld (hl), a
+  inc hl
+  djnz prepare_interior_walls_loop
+  jr prepare_interior_walls
+
+prepare_gnomes
+
+  ld hl, (ix)
+  inc ix
+  inc ix
+  ld a, l
+  or h
+  jr z, prepare_fuel
+
+  call calc_xy_to_hl
+  ex de, hl
+  ld hl, level_buffer
+  add hl, de
+  ld a, GNOME
+  ld (hl), a
+  jr prepare_gnomes
+
+prepare_fuel
+
+  ld hl, (ix)
+  inc ix
+  inc ix
+  ld a, l
+  or h
+  jr z, prepare_dogs
+
+  call calc_xy_to_hl
+  ex de, hl
+  ld hl, level_buffer
+  add hl, de
+  ld a, FUEL
+  ld (hl), a
+  jr prepare_fuel
+
+prepare_dogs
+
+  ret
+
+
+; Converts x,y coordinates in HL to a byte
+; offset for a screen buffer, returned in HL.
+
+calc_xy_to_hl
+
+  push hl
+  pop de
+  ld a, d
+  srl a
+  srl a
+  srl a
+  and 3
+  ld h, a
+  ld a, d
+  sla a
+  sla a
+  sla a
+  sla a
+  sla a
+  ld l, a
+  ld a, e
+  add a, l
+  ld l, a
   ret
 
 ; Game playfield is 32 x 18 (576 bytes)
@@ -181,6 +357,8 @@ prepare_level_side_wall
   BLOCK 0xE000-$, 0xff
 
 level_buffer
+
+  BLOCK 0xE300-$, 0x00
 
 ; Offset table for level data.
 ; Each level is to be found at (Level data + (2 * (level - 1)))
@@ -208,8 +386,8 @@ level_data
 level_1_data
 
   defb 3, 4, 2, 3, 4, 2, 2, 0, 2, 4, 5, 4, 4, 4
-  defb 17, 3, 11, 6, 7, 8, 20, 8, 21, 8, 21, 10, 21, 12, 0, 0
-  defb 8, 4, 24, 6, 16, 6, 22, 8, 15, 9, 12, 10, 8, 11, 20, 12, 14, 13, 17, 14, 0, 0
+  defb 17, 3, 11, 6, 7, 8, 20, 8, 21, 8, 21, 11, 21, 13, 0, 0
+  defb 8, 3, 24, 5, 16, 6, 22, 7, 15, 9, 12, 10, 8, 11, 20, 13, 14, 13, 18, 15, 0, 0
   defb 7, 2, 22, 2, 23, 7, 12, 9, 0, 0
 
 level_2_data
