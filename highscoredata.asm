2575d29a5919f56683873a1930a95515b2a591fc
Brendan Alford <brendan.alford@gmail.com>
2016-08-14 21:45:43 +0100

    Pythonise the High Score table

--- highscoredata.asm
+++ highscoredata.asm
@@ -1,25 +1,26 @@
 high_score_names
 
-  defb "1. ", TAB, 20, INK, 5, "\"Flash, Flash!!!\"",0,"              ", 0
-  defb "2. ", TAB, 20, INK, 5, "\"What is it, Dale? Can't you",0,"   ", 0
-  defb "3. ", TAB, 20, INK, 5, "see I'm eating my bowl",0,"         ", 0
-  defb "4. ", TAB, 20, INK, 5, "of Crunchy Nut Corn",0,"            ", 0
-  defb "5. ", TAB, 20, INK, 5, "Flakes?\"",0,"                       ", 0
-  defb "6. ", TAB, 20, INK, 5, "\"We've only got 15 seconds",0,"     ", 0
-  defb "7. ", TAB, 20, INK, 5, "to save the universe!!!\"",0,"       ", 0
-  defb "8. ", TAB, 20, INK, 5, "\"All right, all right! I'm",0,"     ", 0
-  defb "9. ", TAB, 20, INK, 5, "ready. Now, what's all this",0,"    ", 0
-  defb "10.", TAB, 20, INK, 5, "about a....\"",0,"                   ", 0
+  defb "4. ", TAB, 20, INK, 5, "It's just a flesh wound",0,"        ", 0
+  defb "5. ", TAB, 20, INK, 5, "I'm Brian and so's my wife",0,"     ", 0
+  defb "4. ", TAB, 20, INK, 5, "This is an ex-parrot",0,"           ", 0
+  defb "5. ", TAB, 20, INK, 5, "Blessed are the cheesemakers",0,"   ", 0
+  defb "4. ", TAB, 20, INK, 5, "I fart in your general direction", 0
+  defb "5. ", TAB, 20, INK, 5, "She's a witch!",0,"                 ", 0
+  defb "4. ", TAB, 20, INK, 5, "How shall we f**k off, O Lord?",0," ",0
+  defb "5. ", TAB, 20, INK, 5, "We have no shrubberies here",0,"    ", 0
+  defb "4. ", TAB, 20, INK, 5, "The Castle of Aaarrrrggh",0,"       ", 0
+  defb "5. ", TAB, 20, INK, 5, "He's a very naughty boy",0,"        ", 0
+
 
 high_score_table
 
+  defb "050000", 0, 0
+  defb "045000", 0, 0
+  defb "040000", 0, 0
+  defb "035000", 0, 0
+  defb "030000", 0, 0
   defb "025000", 0, 0
   defb "020000", 0, 0
   defb "015000", 0, 0
   defb "010000", 0, 0
-  defb "007500", 0, 0
   defb "005000", 0, 0
-  defb "004000", 0, 0
-  defb "003000", 0, 0
-  defb "002000", 0, 0
-  defb "001000", 0, 0
